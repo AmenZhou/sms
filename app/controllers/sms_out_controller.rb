4cc1e83001ed6519803b04465639062a5ff6f342
AmenZhou <chou.amen@gmail.com>
2014-04-13 00:51:29 +0000

    finished send basic message function

--- app/controllers/sms_out_controller.rb
+++ app/controllers/sms_out_controller.rb
@@ -3,6 +3,7 @@ class SmsOutController < ApplicationController
   end
 
   def msg_send
+    Phone.send_msg params[:message_body], params[:send_number]
     redirect_to action: :index, notice: 'Message Send'
   end
 end

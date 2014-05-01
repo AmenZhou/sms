b45939bc5a528a4e2ba2efbc790388f8bda59b6e
AmenZhou <chou_amen@hotmail.com>
2014-05-01 00:31:04 +0000

    add thread to sending message

--- app/controllers/sms_out_controller.rb
+++ app/controllers/sms_out_controller.rb
@@ -3,8 +3,8 @@ class SmsOutController < ApplicationController
   end
 
   def msg_send
-    Resque.enqueue SmsJob, params[:message_body], params[:send_number]
-   # Phone.sms_send params[:message_body], params[:send_number]
+    #Resque.enqueue SmsJob, params[:message_body], params[:send_number]
+    Phone.sms_send params[:message_body], params[:send_number]
     flash[:notice] = '正在发送...发送时间比较长，耐心等待。'#String.new
     #notice_arr.each do |notice|
       #flash[:notice] << "Send message to #{notice[:to]}, message content is: #{notice[:body]}.||  "
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -11,43 +11,49 @@ class Phone < ActiveRecord::Base
   end
   #this method is instead of sms_job in works directory 
   def self.sms_send content, to_number
-    account_sid = ENV['TWILIO_ID']
-    auth_token = ENV['TWILIO_TOKEN']
-    max = 69
-    to_numbers = to_number.split(" ")
-    notice_arr = Array.new
-    content_temp = content
-
-    to_numbers.each do |number|
-      next unless phone_validation?(number)
-      while(content.length > 0)
-        if content.length > max
-          cont_split = content[0..(max-1)]
-          content = content[max..-1]
-        else
-          cont_split = content
-          content = ""
-        end
-        @client = Twilio::REST::Client.new account_sid, auth_token
-        sms = @client.account.sms.messages.create(:body => cont_split,
-                                                  :to => number,
-                                                  :from => ENV['TWILIO_PHONE'])
-        puts sms.body 
-
-        notice_hash = Hash.new
-        notice_hash[:to] = number
-        notice_hash[:body] = cont_split
-        notice_arr << notice_hash
-        puts notice_hash
-        sleep(5)
-      end
-      content = content_temp
-    end
-      #puts notice_arr
-    #notice_arr
-  end 
+    background do
+      account_sid = ENV['TWILIO_ID']
+      auth_token = ENV['TWILIO_TOKEN']
+      max = 69
+      to_numbers = to_number.split(" ")
+      notice_arr = Array.new
+      content_temp = content
   
-  #
+      to_numbers.each do |number|
+        next unless phone_validation?(number)
+        while(content.length > 0)
+          if content.length > max
+            cont_split = content[0..(max-1)]
+            content = content[max..-1]
+          else
+            cont_split = content
+            content = ""
+          end
+          @client = Twilio::REST::Client.new account_sid, auth_token
+          sms = @client.account.sms.messages.create(:body => cont_split,
+                                                    :to => number,
+                                                    :from => ENV['TWILIO_PHONE'])
+          puts sms.body 
+  
+          notice_hash = Hash.new
+          notice_hash[:to] = number
+          notice_hash[:body] = cont_split
+          notice_arr << notice_hash
+          puts notice_hash
+          sleep(5)
+        end
+        content = content_temp
+      end
+    end
+  end 
+  #send message in a new thread, background sending
+  def self.background(&block)
+    Thread.new do
+      yield
+      ActiveRecord::Base.connection.close
+    end
+  end
+  #get messages list
   def self.get_messages    
     account_sid = ENV['TWILIO_ID']
     auth_token = ENV['TWILIO_TOKEN']
